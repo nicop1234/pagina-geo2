4663cfb713c9ac8e128daa9ee5d8f14680d3ebb0
nicop1234 <marcelonicolaspereyra@iresm.edu.ar>
2022-10-14 13:54:35 -0300

    mejoras

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 /** @format */
 
 import { Nombres } from "../profile";
+import { useState } from "react";
 import React from "react";
-import { Nav, Navbar, Container, Row } from "react-bootstrap";
-
+import { Nav, Navbar, Container, Row, Collapse, Button } from "react-bootstrap";
+import { IoIosArrowDown } from "react-icons/io";
 const header = ({ children }) => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <Navbar
@@ -13,7 +15,8 @@ const header = ({ children }) => {
         bg='dark'
         variant='dark'
         className='pb-2'
-        sticky='top'>
+        sticky='top'
+      >
         <Container>
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
           <Navbar.Collapse id='responsive-navbar-nav'>
@@ -31,7 +34,8 @@ const header = ({ children }) => {
               <Nav.Link
                 target='_blank'
                 href='https://nosotros-q0clom4pi-faure1.vercel.app'
-                className='text-white fuente'>
+                className='text-white fuente'
+              >
                 About
               </Nav.Link>
             </Nav>
@@ -40,39 +44,57 @@ const header = ({ children }) => {
       </Navbar>
       {children}
 
-      
-        <div className='d-flex bg-dark text-white'>
-          <Container>
-            <Row>
-              <div className='d-flex mx-3 titulo mt-3 mb-3'>
-                <h6 className=' fuente '>Hecho por</h6>
-              </div>
-            </Row>
-            <Row>
-              <Container className='d-flex d-flex justify-content-center'> 
-              <Row className="justify-content-center">
-              {Nombres.map(({ nombre }, i) => (
-                <div className="col-5 col-lg-1 col-md-3 ms-3">
-                  <ul>
-                    <li className='fuente nombres-estilo'>{nombre}</li>
-                  </ul>
+      <div className='d-flex bg-dark text-white'>
+        <Container>
+          <Row>
+            <div className='d-flex mx-3 titulo mt-3 mb-3'>
+              <div className=' d-flex justify-content-center'>
+                <div>
+                  <Button
+                  onClick={() => setOpen(!open)}
+                  aria-controls='example-collapse-text'
+                  aria-expanded={open}
+                  id='Boton-integrntes'
+                  variant="dark"
+                >
+                  Integrantes <IoIosArrowDown></IoIosArrowDown>
+                </Button>
                 </div>
-              ))}
-              </Row>
-              </Container>
-
-            </Row>
-            <Row>
-              <div className='d-flex justify-content-center'>
-                <p>
-                  <span> © </span> DivGeeks Industries /
-                  From SFN
-                </p>
               </div>
-            </Row>
-          </Container>
-        </div>
-    
+            </div>
+          </Row>
+          <Row>
+          <div >
+                  <Collapse in={open}>
+                    <div id='example-collapse-text'>
+                      <div className='d-flex justify-content-center pt-2 '>
+                        <Container className='d-flex d-flex justify-content-center'>
+                          <Row className='justify-content-center'>
+                            {Nombres.map(({ nombre }, i) => (
+                              <div className='col-5 col-lg-1 col-md-3 ms-3'>
+                                <ul>
+                                  <li className='fuente nombres-estilo'>
+                                    {nombre}
+                                  </li>
+                                </ul>
+                              </div>
+                            ))}
+                          </Row>
+                        </Container>
+                      </div>
+                    </div>
+                  </Collapse>
+                </div>
+          </Row>
+          <Row>
+            <div className='d-flex justify-content-center'>
+              <p>
+                <span> © </span> DivGeeks Industries / From SFN
+              </p>
+            </div>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -34,41 +34,54 @@ export default function Home() {
           <Container className='mt-4' id='ques'>
             <Row className=' mb-5'>
               <div>
-                <h1 className='fuente text-center'  >La pesca</h1>
+                <h1 className='fuente text-center'>La pesca</h1>
               </div>
               <div className=' titulo1'></div>
             </Row>
-            <Row classname=' justify-content mt-5 '>
-              <div className='d-flex justify-content-center'>
-                <div className='d-flex col-12 col-lg-5 align-items-center '>
-                  <div className="text-center ">
-                    <p classname='fuente'>
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s, when an unknown
-                      printer took a galley of type and scrambled it to make a
-                      type specimen book. It has survived not only five
-                      centuries, but also the leap into electronic typesetting,
-                      remaining essentially unchanged. It was popularised in the
-                      1960s with the release of Letraset sheets containing Lorem
-                      Ipsum passages, and more recently with desktop publishing
-                      software like Aldus PageMaker including versions of Lorem
-                      Ipsum
-                    </p>
-                  </div>
-                  <div className='d-flex col-lg-1'></div>
+            <Container>
+              <Row>
+                <div className="col-12 col-lg-6 d-flex align-items-center mb-5">
+                  <p className="text-center">
+                  La actividad pesquera y acuícola consiste en la captura y cría de peces, crustáceos, moluscos y otros organismos de aguas saladas y dulces para aprovechar algunos recursos de la Naturaleza sin transformarlos. Estas actividades, además de proporcionar alimento, representan una fuente de ingresos.
+                  </p>
                 </div>
-                <div classname='d-flex col-12 col-lg-5 '>
-                  <Image
+                <div className="col-12 col-lg-6 d-flex align-items-center mb-5">
+                <Image
                     width={1920}
                     height={1159}
                     layout='intrinsic'
                     src='/img/pesca.jpg'
                   />
                 </div>
-              </div>
-            </Row>
+              </Row>
+            </Container>
           </Container>
+          <Container className='mt-4 col-12' id='QuePesca' >
+            <Row className=' mb-5'>
+              <div>
+                <h1 className='fuente text-center'>La pesca</h1>
+              </div>
+              <div className=' titulo1'></div>
+            </Row>
+            <Container>
+              <Row>
+                <div className="col-12 col-lg-6 d-flex align-items-center mb-5">
+                <Image
+                    width={1920}
+                    height={1159}
+                    layout='intrinsic'
+                    src='/img/pesca.jpg'
+                  />
+                </div>
+                <div className="col-12 col-lg-6 d-flex align-items-center mb-5">
+                  <p className="text-center">
+                  Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
+                  </p>
+                </div>
+              </Row>
+            </Container>
+          </Container>
+          
         </Layout>
       </main>
     </>
