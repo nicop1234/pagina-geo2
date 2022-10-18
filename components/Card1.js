9fff2887bbc01069a6b16114ea50cdc540f19355
nicop1234 <101873030+nicop1234@users.noreply.github.com>
2022-10-17 21:37:51 -0300

    Update Card1.js

--- components/Card1.js
+++ components/Card1.js
@@ -7,15 +7,16 @@ const Card1 = ({ direccion, titulo, contenido, boton,link }) => {
   return (
     <div style={{ width: 325 }} class='card'>
       <Image
+      alt="a"
         width={1920}
         height={1159}
         layout='intrinsic'
         src= {direccion}
         />
-      <div class='card-body'>
-        <h5 class='card-title'>{titulo}</h5>
-        <p class='card-text'>{contenido}</p>
-        <a target="_blank" href= {link} class='btn btn-primary'>
+      <div className='card-body'>
+        <h5 className='card-title'>{titulo}</h5>
+        <p className='card-text'>{contenido}</p>
+        <a rel="noopener" href= {link} className='btn btn-primary'>
           {boton}
         </a>
       </div>
